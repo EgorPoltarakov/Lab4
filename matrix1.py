41e978f1cff89c97c4f1b12c9597d02d5fa1d32d
Poltarakov <epoltarakov.99@mail.ru>
2023-01-26 11:14:41 +0300

    Исправленные матрицы

--- matrix1.py
+++ matrix1.py
@@ -1,16 +1,35 @@
-# This is a sample Python script.
+# Open input file
+with open("ESP-zit22-vvod.txt", "r") as input_file:
+    # Get matrix size from file
+    size = int(input_file.readline())
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+    # Throw an error if size is not a natural number
+    if size <= 0:
+        raise ValueError
 
+    count = 0
+    result = 0
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+    for row in range(size):
+        # Get row and convert it into list
+        array = [int(x) for x in input_file.readline().split()]
 
+        # For loop through row
+        for col in range(size):
+            # If item belongs to main diagonal or lies below, skip it
+            if col <= row:
+                continue
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+            # If item is negative, skip it
+            if array[col] < 0:
+                continue
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+            # Add item to result and increase count value by 1
+            result += array[col]
+            count += 1
+
+# Open output file
+with open("ESP-zit22-vivod.txt", "w") as output_file:
+    # Output to file
+    output_file.write(f"Found {count} positive items above main diagonal;\n")
+    output_file.write(f"Sum of all positive items: {result}.")
